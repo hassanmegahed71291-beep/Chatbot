b652b0dec49c1b29b65c503acfe18b3def1cc588
hassanmegahed71291-beep <hassanmegahed71291@gmail.com>
2026-06-25 19:48:52 +0300

    Initialize Firebase with configuration
    Added Firebase configuration and initialization.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
 
+// Firebase Config
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "ضع API Key هنا",
+  authDomain: "qai-platform-7bd96.firebaseapp.com",
+  projectId: "qai-platform-7bd96",
+  storageBucket: "qai-platform-7bd96.firebasestorage.app",
+  messagingSenderId: "970714117363",
+  appId: "1:970714117363:web:a2efd59bf1a5718fa6b386",
+  measurementId: "G-D9JKC385FZ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected Successfully");
+
+export { app };

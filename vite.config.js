88c92306ec3c98715d43455502edfed82df0bfc1
hassanmegahed71291-beep <hassanmegahed71291@gmail.com>
2026-06-26 18:04:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    host: true,
     port: 5173,
     open: true
+  },
+
+  preview: {
+    port: 4173
+  },
+
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 })
